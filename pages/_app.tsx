4380fb5864a51a6fece5e6c3e87ada9180a043a8
Automedon <42299107+Automedon@users.noreply.github.com>
2021-03-13 10:17:07 +0700

    Example of Code : Wrapping App with Reacoil, Styled-Components and React query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
-import '../styles/globals.css'
-import {AppProps} from "next/app";
+import React from "react";
+import { RecoilRoot } from "recoil";
+import "antd/dist/antd.css";
+import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
-function MyApp({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0; 
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const queryClientRef = React.useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default MyApp;
